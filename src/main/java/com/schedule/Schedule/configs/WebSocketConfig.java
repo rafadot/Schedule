66fca41a31ca9e67bf8439eba096cee5a29f4e0f
rafadot1 <rafaelairesdemedeiros@outlook.com>
2023-01-31 11:21:36 -0300

    origins

--- src/main/java/com/schedule/Schedule/configs/WebSocketConfig.java
+++ src/main/java/com/schedule/Schedule/configs/WebSocketConfig.java
@@ -18,6 +18,6 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/ws").setAllowedOrigins("*").withSockJS();
+        registry.addEndpoint("/ws").setAllowedOrigins("3000").withSockJS();
     }
 }
